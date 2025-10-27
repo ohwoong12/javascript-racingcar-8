17b56f287a9ec69c876989e0540c580f86a4c147
OH WOONG KWON <oungtom1212@naver.com>
2025-10-27 21:48:42 +0900

    feat: 문자열 상수 객체 수정
    - 새로운 에러 메시지를 추가

--- src/Constants.js
+++ src/Constants.js
@@ -4,4 +4,9 @@ export const CONSOLE_MESSAGE = {
   SELECT_TRY_NUMBER: '시도할 횟수는 몇 회인가요?',
 };
 
-
+export const ERROR_MESSAGE = {
+  INVALID_NAME_LENGTH: '[ERROR] 이름은 5자 이하만 가능합니다.',
+  INVALID_CAR_NAME_TYPE: '[ERROR] 자동차 이름이 제대로 입력되지 않았습니다.',
+  INVALID_CAR_NAME_EMPTY: '[ERROR] 자동차 이름이 공백일 수 없습니다.',
+  INVALID_TRY_NUMBER_RANGE: '[ERROR] 시도할 횟수는 양수만 입력 가능합니다.',
+};
